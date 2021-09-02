07d64ec81ba505eb9c5fa4daa0ed805c3a799cab
willbruto22 <89882559+willbruto22@users.noreply.github.com>
2021-09-01 22:42:07 -0300

    Add files via upload

---
+++ Class11.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace POOtriangulo
+{
+    internal class Triangulo
+    {
+        internal double A;
+        internal double B;
+        internal double C;
+    }
+}
